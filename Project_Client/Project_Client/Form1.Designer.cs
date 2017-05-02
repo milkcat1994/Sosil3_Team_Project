155c9a4f67f8015c55836c6f0fe0e3d0472c51bb
YangJiYong <milkcat1994@gmail.com>
2017-05-02 15:10:51 +0900

    Make Login DB

--- Project_Client/Project_Client/Form1.Designer.cs
+++ Project_Client/Project_Client/Form1.Designer.cs
@@ -29,40 +29,43 @@
         private void InitializeComponent()
         {
             this.panel_ID = new System.Windows.Forms.Panel();
-            this.panel_PW = new System.Windows.Forms.Panel();
-            this.label_ID = new System.Windows.Forms.Label();
-            this.panel_IP = new System.Windows.Forms.Panel();
-            this.panel_Port = new System.Windows.Forms.Panel();
-            this.label_PW = new System.Windows.Forms.Label();
-            this.label_IP = new System.Windows.Forms.Label();
-            this.label_Port = new System.Windows.Forms.Label();
             this.textBox_ID = new System.Windows.Forms.TextBox();
+            this.label_ID = new System.Windows.Forms.Label();
+            this.panel_PW = new System.Windows.Forms.Panel();
             this.textBox_PW = new System.Windows.Forms.TextBox();
+            this.label_PW = new System.Windows.Forms.Label();
+            this.panel_IP = new System.Windows.Forms.Panel();
             this.textBox_IP = new System.Windows.Forms.TextBox();
+            this.label_IP = new System.Windows.Forms.Label();
+            this.panel_Port = new System.Windows.Forms.Panel();
             this.textBox_Port = new System.Windows.Forms.TextBox();
+            this.label_Port = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel_First = new System.Windows.Forms.Panel();
             this.panel_ID.SuspendLayout();
             this.panel_PW.SuspendLayout();
             this.panel_IP.SuspendLayout();
             this.panel_Port.SuspendLayout();
+            this.panel_First.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel_ID
             // 
             this.panel_ID.Controls.Add(this.textBox_ID);
             this.panel_ID.Controls.Add(this.label_ID);
-            this.panel_ID.Location = new System.Drawing.Point(128, 178);
+            this.panel_ID.Location = new System.Drawing.Point(34, 83);
             this.panel_ID.Name = "panel_ID";
             this.panel_ID.Size = new System.Drawing.Size(171, 25);
             this.panel_ID.TabIndex = 0;
             // 
-            // panel_PW
+            // textBox_ID
             // 
-            this.panel_PW.Controls.Add(this.textBox_PW);
-            this.panel_PW.Controls.Add(this.label_PW);
-            this.panel_PW.Location = new System.Drawing.Point(118, 209);
-            this.panel_PW.Name = "panel_PW";
-            this.panel_PW.Size = new System.Drawing.Size(181, 25);
-            this.panel_PW.TabIndex = 1;
+            this.textBox_ID.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_ID.Location = new System.Drawing.Point(24, 0);
+            this.textBox_ID.Name = "textBox_ID";
+            this.textBox_ID.Size = new System.Drawing.Size(147, 21);
+            this.textBox_ID.TabIndex = 1;
             // 
             // label_ID
             // 
@@ -74,23 +77,22 @@
             this.label_ID.TabIndex = 0;
             this.label_ID.Text = "ID :";
             // 
-            // panel_IP
+            // panel_PW
             // 
-            this.panel_IP.Controls.Add(this.textBox_IP);
-            this.panel_IP.Controls.Add(this.label_IP);
-            this.panel_IP.Location = new System.Drawing.Point(12, 119);
-            this.panel_IP.Name = "panel_IP";
-            this.panel_IP.Size = new System.Drawing.Size(196, 25);
-            this.panel_IP.TabIndex = 2;
+            this.panel_PW.Controls.Add(this.textBox_PW);
+            this.panel_PW.Controls.Add(this.label_PW);
+            this.panel_PW.Location = new System.Drawing.Point(56, 144);
+            this.panel_PW.Name = "panel_PW";
+            this.panel_PW.Size = new System.Drawing.Size(181, 25);
+            this.panel_PW.TabIndex = 1;
             // 
-            // panel_Port
+            // textBox_PW
             // 
-            this.panel_Port.Controls.Add(this.textBox_Port);
-            this.panel_Port.Controls.Add(this.label_Port);
-            this.panel_Port.Location = new System.Drawing.Point(214, 119);
-            this.panel_Port.Name = "panel_Port";
-            this.panel_Port.Size = new System.Drawing.Size(171, 25);
-            this.panel_Port.TabIndex = 2;
+            this.textBox_PW.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_PW.Location = new System.Drawing.Point(31, 0);
+            this.textBox_PW.Name = "textBox_PW";
+            this.textBox_PW.Size = new System.Drawing.Size(150, 21);
+            this.textBox_PW.TabIndex = 2;
             // 
             // label_PW
             // 
@@ -102,6 +104,23 @@
             this.label_PW.TabIndex = 1;
             this.label_PW.Text = "PW :";
             // 
+            // panel_IP
+            // 
+            this.panel_IP.Controls.Add(this.textBox_IP);
+            this.panel_IP.Controls.Add(this.label_IP);
+            this.panel_IP.Location = new System.Drawing.Point(32, 19);
+            this.panel_IP.Name = "panel_IP";
+            this.panel_IP.Size = new System.Drawing.Size(196, 25);
+            this.panel_IP.TabIndex = 2;
+            // 
+            // textBox_IP
+            // 
+            this.textBox_IP.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_IP.Location = new System.Drawing.Point(24, 0);
+            this.textBox_IP.Name = "textBox_IP";
+            this.textBox_IP.Size = new System.Drawing.Size(172, 21);
+            this.textBox_IP.TabIndex = 3;
+            // 
             // label_IP
             // 
             this.label_IP.AutoSize = true;
@@ -111,6 +130,23 @@
             this.label_IP.Size = new System.Drawing.Size(24, 12);
             this.label_IP.TabIndex = 2;
             this.label_IP.Text = "IP :";
+            // 
+            // panel_Port
+            // 
+            this.panel_Port.Controls.Add(this.textBox_Port);
+            this.panel_Port.Controls.Add(this.label_Port);
+            this.panel_Port.Location = new System.Drawing.Point(224, 70);
+            this.panel_Port.Name = "panel_Port";
+            this.panel_Port.Size = new System.Drawing.Size(171, 25);
+            this.panel_Port.TabIndex = 2;
+            // 
+            // textBox_Port
+            // 
+            this.textBox_Port.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_Port.Location = new System.Drawing.Point(35, 0);
+            this.textBox_Port.Name = "textBox_Port";
+            this.textBox_Port.Size = new System.Drawing.Size(136, 21);
+            this.textBox_Port.TabIndex = 4;
             // 
             // label_Port
             // 
@@ -122,47 +158,45 @@
             this.label_Port.TabIndex = 3;
             this.label_Port.Text = "Port :";
             // 
-            // textBox_ID
+            // button1
             // 
-            this.textBox_ID.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox_ID.Location = new System.Drawing.Point(24, 0);
-            this.textBox_ID.Name = "textBox_ID";
-            this.textBox_ID.Size = new System.Drawing.Size(147, 21);
-            this.textBox_ID.TabIndex = 1;
+            this.button1.Location = new System.Drawing.Point(12, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox_PW
+            // button2
             // 
-            this.textBox_PW.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox_PW.Location = new System.Drawing.Point(31, 0);
-            this.textBox_PW.Name = "textBox_PW";
-            this.textBox_PW.Size = new System.Drawing.Size(150, 21);
-            this.textBox_PW.TabIndex = 2;
+            this.button2.Location = new System.Drawing.Point(110, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // textBox_IP
+            // panel_First
             // 
-            this.textBox_IP.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox_IP.Location = new System.Drawing.Point(24, 0);
-            this.textBox_IP.Name = "textBox_IP";
-            this.textBox_IP.Size = new System.Drawing.Size(172, 21);
-            this.textBox_IP.TabIndex = 3;
-            // 
-            // textBox_Port
-            // 
-            this.textBox_Port.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox_Port.Location = new System.Drawing.Point(35, 0);
-            this.textBox_Port.Name = "textBox_Port";
-            this.textBox_Port.Size = new System.Drawing.Size(136, 21);
-            this.textBox_Port.TabIndex = 4;
+            this.panel_First.Controls.Add(this.panel_IP);
+            this.panel_First.Controls.Add(this.panel_Port);
+            this.panel_First.Controls.Add(this.panel_ID);
+            this.panel_First.Controls.Add(this.panel_PW);
+            this.panel_First.Location = new System.Drawing.Point(12, 31);
+            this.panel_First.Name = "panel_First";
+            this.panel_First.Size = new System.Drawing.Size(404, 367);
+            this.panel_First.TabIndex = 2;
             // 
             // Form_Client
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(397, 290);
-            this.Controls.Add(this.panel_IP);
-            this.Controls.Add(this.panel_Port);
-            this.Controls.Add(this.panel_PW);
-            this.Controls.Add(this.panel_ID);
+            this.ClientSize = new System.Drawing.Size(424, 407);
+            this.Controls.Add(this.panel_First);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "Form_Client";
             this.Text = "Client";
             this.panel_ID.ResumeLayout(false);
@@ -173,6 +207,7 @@
             this.panel_IP.PerformLayout();
             this.panel_Port.ResumeLayout(false);
             this.panel_Port.PerformLayout();
+            this.panel_First.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -191,6 +226,9 @@
         private System.Windows.Forms.Panel panel_Port;
         private System.Windows.Forms.TextBox textBox_Port;
         private System.Windows.Forms.Label label_Port;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel_First;
     }
 }
 
--- Project_Client/Project_Client/Form1.cs
+++ Project_Client/Project_Client/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Project_Client
 {
@@ -15,6 +16,33 @@ namespace Project_Client
         public Form_Client()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlCon = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = D:\school\3\Linux\Project\Project_Server\Project_Server\Server_DB.mdf; Integrated Security = True; Connect Timeout = 30");
+            string query = "select * from Login where Id ='" + textBox_ID.Text.Trim() + "'and password = '" + textBox_PW.Text.Trim() + "'";
+            SqlDataAdapter sqla = new SqlDataAdapter(query, sqlCon);
+            DataTable dt = new DataTable();
+            sqla.Fill(dt);
+
+            if (dt.Rows.Count == 1)
+            {
+                panel_First.Visible = false;
+                Form_Client.ActiveForm.Width = 200;
+                Form_Client.ActiveForm.Height = 200;
+            }
+            else
+            {
+                MessageBox.Show("re write!! pw");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form_Client.ActiveForm.Width = 600;
+            Form_Client.ActiveForm.Height = 600;
+            panel_First.Visible = true;
         }
     }
 }
