3ef00471e8cd51ab74ae2df6be9aaf6886a78674
YangJiYong <milkcat1994@gmail.com>
2017-06-01 23:26:27 +0900

    success multiClient

---
+++ 참고자료/Listener.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketProgram00Server_MultipleConnection2
+{
+    class Listener
+    {
+        Socket s;
+        public bool Listening
+        {
+            get;
+            private set;
+        }
+
+        public int Port
+        {
+            get;
+            private set;
+        }
+
+        public Listener(int port)
+        {
+            Port = port;
+            s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public void Start(string tcpServerIP)
+        {
+            if (Listening)
+                return;
+
+            s.Bind(new IPEndPoint(IPAddress.Parse(tcpServerIP), Port));            
+            //s.Bind(new IPEndPoint(0, Port));
+            s.Listen(0);
+
+            s.BeginAccept(callback, null);
+            Listening = true;
+        }
+
+        public void Stop()
+        {
+            if (!Listening)
+                return;
+
+            s.Close();
+            s.Dispose();
+            Listening = false;
+
+            s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        void callback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket s = this.s.EndAccept(ar);
+
+                if (SocketAccepted != null)
+                {
+                    SocketAccepted(s);
+                }
+
+                this.s.BeginAccept(callback, null);
+            }   
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public delegate void SocketAcceptedHandler(Socket e);
+        public event SocketAcceptedHandler SocketAccepted;        
+    }
+}
